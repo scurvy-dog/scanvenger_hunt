efa2f0fc0b5affcca2462aa030e74157b53c7f76
scurvy-dog <robermur@cisco.com>
2017-01-16 15:28:40 -0800

    Changed room to use a class object with method

--- room.py
+++ room.py
@@ -1,33 +1,41 @@
-# Room Definitions for Scavenger Hunt
+class Room:
+	def __init__ (self):
+		self.id = None
+		self.description = None
+		self.item = None
+		self.monster = None
+		self.puzzle = None
+		self.exits = None
 
-# Define Variables
+	def loadroom(self,rm_number):
+		__filename = "room_db.txt"
+		__room_match = "Room: " + str(rm_number)
+		__switch = False
+		self.id = rm_number
+		x = []
 
-def load_room(rm_number):
-	filename = "room_db.txt"
-	rm_number = "Room: " + str(rm_number)
-	switch = False
-	x = []
+		# Open room file
+		with open(__filename) as fobject:
+			rlist = fobject.read().splitlines()
 
-	# Open room file
-	with open(filename) as fobject:
-		rlist = fobject.read().splitlines()
-
-	for item in rlist:
-  		if item == rm_number:
-			# True when we find the correct room number
-			switch = True
-  		if switch:
-			if not item:
-				# Blank line will be end of room information
-				switch = False
-				# Stop reading items from rlist and return function
-				break
- 			# remove field description into v and value to y
-			print item
-			v,y = item.split(':',1)
-			# remove leading or trailing white space
-			y=y.strip()
-			# Append values to list
-			x.append(y)
-			# Blank line will be end of room information
-	return(x)
+		for data in rlist:
+	  		if data == __room_match:
+				__switch = True
+			if __switch:
+				if data.startswith('Description:'):
+					v,y = data.split(':',1)
+					self.description = y.strip()
+				elif data.startswith('Item:'):
+					v,y = data.split(':',1)
+					self.data = y.strip()
+				elif data.startswith('Monster:'):
+					v,y = data.split(':',1)
+					self.monster = y.strip()
+				elif data.startswith('Puzzle:'):
+					v,y = data.split(':',1)
+					self.puzzle = y.strip()
+				elif data.startswith('Move:'):
+					v,y = data.split(':',1)
+					self.exits = y.strip()
+				elif not data:
+					break
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-from rtest import *
-y=[]
+from room import *
 
-y = load_room(1)
+cRoom = Room()
 
-print(y)
+cRoom.loadroom(1)
+
+print cRoom.id
+print cRoom.description
+print cRoom.item
+print cRoom.monster
+print cRoom.puzzle
+print cRoom.exits
