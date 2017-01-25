7f0e6e29ab7a7099e228a87af37a3dbd4851e9d6
Michyfish <murphy.micheline@gmail.com>
2017-01-24 18:01:54 -0800

    This is the function for loading monster information.

---
+++ load_monster.py
@@ -0,0 +1,30 @@
+
+#Load Monster Information from the Monster Database
+def load_monster(monster_number):
+	filename = "monster_db.txt"
+	monster = "Monster: " + str(monster_number)
+	switch = False
+	x = []
+	
+	#Open Monster Database
+	with open(filename) as fobject:
+		rlist = fobject.read().splitlines()
+		
+	#Scan Item Database for the correct Monster Num
+	for item in rlist:
+		if item == monster_number:
+			switch = True
+	
+	#Retrieve Item Description	
+	if switch == True:
+		if not item:
+			switch = False
+			break
+			
+		v,y = item.split(':', 1)
+		y = y.strip()
+		x.append(y)
+	
+	
+	#Returning array with Monster Name, Description HP, trophy
+	return(x)					
